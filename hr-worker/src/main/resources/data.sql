11ca2643c96ef064ac1e31e0d4f71fcccdcededd
Ayame Hinode <ayamehinode@gmail.com>
2023-09-02 23:40:33 -0300

    Fix Creation of tables order

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,9 +1,3 @@
-CREATE TABLE TB_WORKERS(
-        ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-        NAME VARCHAR(150),
-        DAILY_INCOME NUMERIC(20,2)
-);
-
 INSERT INTO tb_workers (name, daily_Income) VALUES ('X', 300.0);
 INSERT INTO tb_workers (name, daily_Income) VALUES ('Zero', 350.0);
 INSERT INTO tb_workers (name, daily_Income) VALUES ('Axl', 250.0);
