b27c1dfa0bebbd06e0cb5612ca6f92be074b9537
Ayame Hinode <ayamehinode@gmail.com>
2023-09-06 22:43:15 -0300

    Feign Client implement in the hr-payroll

--- hr-payroll/src/main/java/com/repliforce/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/repliforce/hrpayroll/resources/PaymentResource.java
@@ -2,7 +2,6 @@ package com.repliforce.hrpayroll.resources;
 
 import com.repliforce.hrpayroll.entities.Payment;
 import com.repliforce.hrpayroll.servces.PaymentService;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
