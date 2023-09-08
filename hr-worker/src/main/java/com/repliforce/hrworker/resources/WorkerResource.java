24b08809fd0a51843d68cf7b4dd50c7bf91976ba
Ayame Hinode <ayamehinode@gmail.com>
2023-09-07 23:30:46 -0300

    Ribbon implemented, hr-worker and hr-payroll conected

--- hr-worker/src/main/java/com/repliforce/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/repliforce/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.repliforce.hrworker.resources;
 
 import com.repliforce.hrworker.entities.Worker;
 import com.repliforce.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,14 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger printLogger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment enviroment;
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +37,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public  ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        printLogger.info("PORT = " + enviroment.getProperty("local.server.port"));
+
         Worker worker = repository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
