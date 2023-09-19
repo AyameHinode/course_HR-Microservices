2f01fd8d69203a31d515d73ce1a330c575a9d619
Ayame Hinode <ayamehinode@gmail.com>
2023-09-19 20:44:38 -0300

    Project hr-worker defined as config server client

--- hr-worker/src/main/java/com/repliforce/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/repliforce/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.repliforce.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class WorkerResource {
 
     private static Logger printLogger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment enviroment;
 
@@ -28,6 +32,12 @@ public class WorkerResource {
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        printLogger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
